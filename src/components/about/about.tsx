116096b64f8467bd16e1e22023232d99f9c7f9f1
Jesus <jesus.cassano.2006@gmail.com>
2023-03-19 13:14:06 +0100

    📝 Create about page file

---
+++ src/components/about/about.tsx
@@ -0,0 +1,45 @@
+export default function AboutPage(): JSX.Element {
+  return (
+    <section className="container__about">
+      <div className="main__title">
+        <h2>About us</h2>
+      </div>
+
+      <div>
+        <h3 className="about__title">NUESTRO PALMARES</h3>
+        <p className="about__titletext">
+          * TROFEO FIFA AL MEJOR CLUB DEL SIGLO XX * 14 COPAS DE EUROPA * 5
+          SUPERCOPAS DE EUROPA * 8 MUNDIALES DE CLUBES * 35 LIGAS * 19 COPAS DEL
+          REY * 12 SUPERCOPAS DE ESPAÑA
+        </p>
+      </div>
+
+      <div>
+        <h3 className="about__title">PROPOSITO</h3>
+        <p className="about__titletext">
+          Mejorar vidas utilizando el deporte para educar en valores, integrar y
+          contribuir al desarrollo de la sociedad.
+        </p>
+      </div>
+
+      <div>
+        <h3 className="about__title">MISION</h3>
+        <p className="about__titletext">
+          Expresar mediante su acción social, el compromiso solidario del Real
+          Madrid; preservar y conservar el patrimonio histórico del club;
+          fomentar los valores inherentes al deporte como su principal activo,
+          para favorecer la educación integral de la infancia y la juventud, así
+          como la inclusión social de los más vulnerables.
+        </p>
+      </div>
+
+      <div>
+        <h3 className="about__title">VISION</h3>
+        <p className="about__titletext">
+          Ser un referente universal en el uso del deporte como herramienta
+          educativa y de integración social.
+        </p>
+      </div>
+    </section>
+  );
+}
