c83eb0f72253767de55504d8d63e0d3ee8b79d47
Jesus <jesus.cassano.2006@gmail.com>
2023-03-20 17:17:21 +0100

    📝 Add changes slice file

--- src/reducer/users.slice.ts
+++ src/reducer/users.slice.ts
@@ -19,7 +19,6 @@ export const userSlice = createSlice({
       state.users = [...state.users, action.payload];
     },
     login(state, action: PayloadAction<User>) {
-      debugger;
       state.userLogged = action.payload;
     },
   },
