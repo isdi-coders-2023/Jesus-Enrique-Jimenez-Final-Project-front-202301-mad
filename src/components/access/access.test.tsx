87bc24f7174c7fdf050478e588f98603c989480a
Jesus <jesus.cassano.2006@gmail.com>
2023-03-20 18:52:23 +0100

    🐛 Fix line in access test

--- src/components/access/access.test.tsx
+++ src/components/access/access.test.tsx
@@ -1,5 +1,4 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import { useState } from 'react';
 import { Provider } from 'react-redux';
 import { store } from '../../store/store';
 import Register from '../register/register';
@@ -7,21 +6,16 @@ import Access from './access';
 
 jest.mock('../register/register');
 
-beforeEach(() => {
-  // eslint-disable-next-line testing-library/no-render-in-setup
-  render(
-    <Provider store={store}>
-      <Access></Access>
-    </Provider>
-  );
-});
-
 describe('Given the access component', () => {
-  describe('when we render it', () => {
-    test('then register have been called', () => {
-      expect(Register).toHaveBeenCalled();
-    });
+  beforeEach(() => {
+    // eslint-disable-next-line testing-library/no-render-in-setup
+    render(
+      <Provider store={store}>
+        <Access></Access>
+      </Provider>
+    );
   });
+
   describe('When we check the buttons', () => {
     test('then they should be in the document', () => {
       const button = screen.getAllByRole('button');
