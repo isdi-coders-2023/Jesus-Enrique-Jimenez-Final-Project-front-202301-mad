ef45e98b1fbe23cd108a0b60f9366c68995985cb
Jesus <jesus.cassano.2006@gmail.com>
2023-03-23 12:36:34 +0100

    📝 Create login files

--- src/components/login/login.tsx
+++ src/components/login/login.tsx
@@ -1,40 +1,49 @@
 import { SyntheticEvent, useMemo } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { useUsers } from '../../hooks/use.users';
 import { User } from '../../models/users';
-import { UsersRepo } from '../../services/users.repo';
+import { UsersApiRepo } from '../../services/users.api.repo';
+import styles from './login.module.scss';
 
 export default function Login() {
-  const repo = useMemo(() => new UsersRepo(), []);
+  const navigate = useNavigate();
 
-  const { userLogin } = useUsers(repo);
+  const UserRepo = useMemo(() => new UsersApiRepo(), []);
+
+  const { userLogin } = useUsers(UserRepo);
 
   const handleSubmit = (ev: SyntheticEvent<HTMLFormElement>) => {
     ev.preventDefault();
 
-    const formData = ev.currentTarget;
-    const userLog: Partial<User> = {
-      userName: (formData.elements[0] as HTMLFormElement).value,
-      password: (formData.elements[1] as HTMLFormElement).value,
+    const formLoginUser = ev.currentTarget;
+
+    const logUser: Partial<User> = {
+      userName: (formLoginUser.elements[0] as HTMLFormElement).value,
+      password: (formLoginUser.elements[1] as HTMLFormElement).value,
     };
 
-    userLogin(userLog);
-    console.log(userLog);
-    formData.reset();
+    userLogin(logUser);
+
+    formLoginUser.reset();
+    navigate('/home');
   };
-
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Username
-        <input type="text" name="userName" required />
-      </label>
+    <div className={styles.login}>
+      <form className={styles.input} onSubmit={handleSubmit}>
+        <label>
+          Username
+          <input type="text" name="userName" required />
+        </label>
 
-      <label>
-        Password
-        <input type="password" name="password" required />
-      </label>
+        <label>
+          Password
+          <input type="password" name="password" required />
+        </label>
 
-      <button type="submit">Login</button>
-    </form>
+        <button className={styles.button} type="submit">
+          Login
+        </button>
+      </form>
+    </div>
   );
 }
