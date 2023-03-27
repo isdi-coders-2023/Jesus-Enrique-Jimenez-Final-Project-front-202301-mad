823f7f7ca7f3cb21574257cbc179cfe8d15db076
Jesus <jesus.cassano.2006@gmail.com>
2023-03-28 00:38:09 +0200

    ✅ Add edit page test

---
+++ src/components/edit-player/edit.tsx
@@ -0,0 +1,11 @@
+import Form from '../form/form';
+import '../create/create.module.scss';
+
+export default function Edit() {
+  return (
+    <>
+      <h2>Edita tu jugador</h2>
+      <Form></Form>
+    </>
+  );
+}
