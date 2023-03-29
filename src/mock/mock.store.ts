88eac5e0b1f5b6b57ebf71f98d8029a585c5511d
Jesus <jesus.cassano.2006@gmail.com>
2023-03-29 09:15:28 +0200

    📝 Create mock store file

---
+++ src/mock/mock.store.ts
@@ -0,0 +1,35 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { userInfo } from 'os';
+import { User } from '../models/users';
+import { playerReducer } from '../reducer/players.slice';
+import { userReducer } from '../reducer/users.slice';
+import { RootState } from '../store/store';
+const mockPlayer = {
+  id: '',
+  name: '',
+  nationality: '',
+  age: 30,
+  position: '',
+  preferredFoot: '',
+  picture: '',
+  creator: '',
+};
+export const preloadedState: Partial<RootState> = {
+  users: {
+    userLogged: null as unknown as User,
+    allUsers: [],
+    user: {} as User,
+  },
+  players: {
+    allPlayers: [mockPlayer],
+    player: mockPlayer,
+  },
+};
+
+export const mockStore = configureStore({
+  reducer: {
+    users: userReducer,
+    players: playerReducer,
+  },
+  preloadedState,
+});
